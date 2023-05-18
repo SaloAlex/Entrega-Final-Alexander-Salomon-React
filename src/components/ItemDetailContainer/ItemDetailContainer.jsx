533c826f7676e75fff2d72b0f6bd8f9e767695e4
Alexander Salomon <dvdsalomon6@gmail.com>
2023-05-18 15:59:06 -0300

    Corregir errores

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,40 +1,34 @@
-import React from 'react'
-import ItemDetail from './ItemDetail'
-import Spinner from '../Spinner/Spinner'
-import { useState, useEffect } from 'react'
-import { getSingleProduct } from '../../services/firestore'
-import { useParams } from 'react-router-dom'
-
-
+import React from 'react';
+import ItemDetail from './ItemDetail';
+import Spinner from '../Spinner/Spinner';
+import { useState, useEffect, useCallback } from 'react';
+import { getSingleProduct } from '../../services/firestore';
+import { useParams } from 'react-router-dom';
 
 function ItemDetailContainer() {
+  const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
 
-    const [product, setProduct] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const { id } = useParams();
-
-    async function getSingleProductAsync() {
-        try {
-            getSingleProduct(id).then (respuesta => {
-                setProduct(respuesta);
-                setIsLoading(false);
-            });
-        } catch (error) {
-            console.error('Error desde la base de datos', error);
-        }
+  const getSingleProductAsync = useCallback(async () => {
+    try {
+      const respuesta = await getSingleProduct(id);
+      setProduct(respuesta);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error desde la base de datos', error);
     }
+  }, [id]);
 
-    useEffect(() => {
-        getSingleProductAsync();
-    }, []);
+  useEffect(() => {
+    getSingleProductAsync();
+  }, [getSingleProductAsync]);
 
-    if (isLoading) {
-        return (<Spinner />);
-    }
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-    return (
-        <ItemDetail product={product} />
-    )
+  return <ItemDetail product={product} />;
 }
 
 export default ItemDetailContainer;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import ItemList from './ItemList/ItemList'
-import Spinner from '../Spinner/Spinner'
-import getProducts, { getProductsByCategory } from '../../services/firestore'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import ItemList from './ItemList/ItemList';
+import Spinner from '../Spinner/Spinner';
+import getProducts, { getProductsByCategory } from '../../services/firestore';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 
 function ItemListContainer() {
+  const [products, setProducts] = useState(null);
+  const { idCategory } = useParams();
 
-    const [products, setProducts] = useState(null);
-    const { idCategory } = useParams();
-
-    async function getProductsAsync() {
-        if (idCategory) {
-            let respuesta = await getProductsByCategory(idCategory);
-            setProducts(respuesta);
-        } else {
-            let respuesta = await getProducts();
-            setProducts(respuesta);
-        }
+  const getProductsAsync = useCallback(async () => {
+    if (idCategory) {
+      const respuesta = await getProductsByCategory(idCategory);
+      setProducts(respuesta);
+    } else {
+      const respuesta = await getProducts();
+      setProducts(respuesta);
     }
+  }, [idCategory]);
 
-    useEffect(() => {
-        getProductsAsync();
-    }, [idCategory]);
+  useEffect(() => {
+    getProductsAsync();
+  }, [getProductsAsync]);
 
-    return <> {
-        products ? <ItemList products={products} /> : <Spinner />
-    }
-    </>
+  return <> {products ? <ItemList products={products} /> : <Spinner />} </>;
 }
 
 export default ItemListContainer;
